be8a84e38008bacd7104b7b02d9c60399c08e5b7
Protasov Sergey <protasov.sa@phystech.edu>
2021-10-05 23:25:46 +0300

    Pluz

--- Pluz.py
+++ Pluz.py
@@ -1,24 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 def pluz(arg1, arg2):
-    arg = 'Эти объекты нельзя сложить!'
     try:
         arg = arg1 + arg2
-    except Exception :
+        return arg
+    except TypeError :
         pass
     try:
         arg = int(arg1) + arg2
-    except Exception :
+        return arg
+    except TypeError :
         pass
     try:
         arg = arg1 + int(arg2)
-    except Exception :
+        return arg
+    except TypeError :
         pass
-    return arg
+    return arg1 + arg2
+
 print(pluz(arg1, arg2))
-# Вместо arg1 и arg2 должны стоять вводные
+
 
