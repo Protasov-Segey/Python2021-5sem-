955ef1404b3cf2637d5a1965b6ebd31ba2bf7fd1
Protasov Sergey <protasov.sa@phystech.edu>
2021-11-23 18:13:26 +0300

    Hw10, 2 problem (with dict)

--- Hw10_2.py
+++ Hw10_2.py
@@ -3,19 +3,21 @@ import sys
 import threading
 
 def thread_job(file, string, num):
+    global d 
+    d[books[num]] = [] 
     with open(file, 'r') as book:
         while True:
             line = book.readline()
             if not line:
                 break
             if string in line:
-                print(f'{books[num]}:', line)
+                d[books[num]].append(line)
         sys.stdout.flush()
 
 s = str(sys.argv[1])
 directory = str(sys.argv[2])
 books = os.listdir(directory)
-
+d = dict()
 if __name__=='__main__':
     threads = [
         threading.Thread(target=thread_job, args=(os.path.join(directory, books[i]), s, i))
@@ -25,3 +27,6 @@ if __name__=='__main__':
         thread.start()  
     for thread in threads:
         thread.join()  
+    for head, lines in d.items():
+        for line in lines:
+            print(f'{head}:', line)
